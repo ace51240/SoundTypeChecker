b816859c18118679084223d047328c9105884678
Takahiro Yano <takahiro.yano@optim.co.jp>
2021-06-03 23:04:27 +0900

    アプリ起動時に通知ポリシーのパーミッション設定をさせるように修正 RingとNotificationは上記のパーミッションなしに0にできない。

--- app/src/main/java/com/example/soundtypechecker/MainActivity.java
+++ app/src/main/java/com/example/soundtypechecker/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.soundtypechecker;
 
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -33,6 +36,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = getApplicationContext();
+
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !notificationManager.isNotificationPolicyAccessGranted()) {
+
+            Intent intent = new Intent(
+                    android.provider.Settings
+                            .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+
+            startActivity(intent);
+        }
 
         audioTypeManager = new AudioTypeManager(this);
 
